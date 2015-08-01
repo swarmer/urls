5cbdb0e300d70f17f58f453bd5fae5c951140055
Anton Barkovsky <anton@swarmer.me>
2015-08-01 16:21:48 +0300

    Show users only their own links in admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,44 @@
 from django.contrib import admin
 
-from . import models
+from .models import Link
 
 
-admin.site.register(models.Link)
+class LinkAdmin(admin.ModelAdmin):
+    def get_changeform_initial_data(self, request):
+        return {'owner': request.user}
+
+    def get_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return ['name', 'target', 'owner']
+        else:
+            return ['name', 'target']
+
+    def has_change_permission(self, request, obj=None):
+        has_class_permission = super().has_change_permission(request, obj)
+        if not has_class_permission:
+            return False
+
+        if obj is None:
+            return True
+
+        owns_object = request.user.id == obj.owner.id
+        if request.user.is_superuser or owns_object:
+            return True
+
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return self.has_change_permission(request, obj)
+
+    def get_queryset(self, request):
+        if request.user.is_superuser:
+            return Link.objects.all()
+        return Link.objects.filter(owner=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.owner = request.user
+        obj.save()
+
+
+admin.site.register(Link, LinkAdmin)
--- core/models.py
+++ core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 
 class Link(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, default=None)
     name = models.CharField(max_length=100, unique=True)
     target = models.URLField()
 
